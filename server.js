e14f5a121745d40eee35fdf8e65eae9cb321ebe9
Calvin Maguranis <cmaguranis@quickbase.com>
2018-02-20 12:36:10 -0500

    Fixed backend functionality by adding back in accidentally deleted JSON DB and body parser middleware.

--- server.js
+++ server.js
@@ -17,12 +17,17 @@ app.use(webpackDevMiddleware(
   historyApiFallback: true,
 }));
 
+// placeholder for a real DB
+let awesomejsondb = {};
+
 function getusercount(db, name) { return { [name]: db[name]}}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/count', (req, res) => res.json(awesomejsondb))
   .post('/count', (req, res) => {
     const name = req.body.name;
-    console.log(name);
 
     // find if name is already in the db
     if (awesomejsondb[name]) {
@@ -34,16 +39,9 @@ app.get('/count', (req, res) => res.json(awesomejsondb))
     return res.json(getusercount(awesomejsondb, name));
   });
   
-app.get('/count/:name', (req, res) => {
-  console.log(req.params)
-  const name = req.params.name;
-  return res.json(getusercount(awesomejsondb, name));
-});
-  
 app.use(express.static(__dirname + '/www'));
 
 const server = app.listen(3000, function() {
-  const host = server.address().address;
   const port = server.address().port;
-  console.log('Server being started at http://localhost:',port);
+  console.log(`Server being started at localhost:${port}`);
 });
